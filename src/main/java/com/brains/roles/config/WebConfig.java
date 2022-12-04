f95b7751830216a98985fb36b3e7b6839673b578
olamide <myyde8170@gmail.com>
2022-12-04 16:32:26 +0100

    initial commit

---
+++ src/main/java/com/brains/roles/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.brains.roles.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(getEncoder().encode("pass1")).roles()
+                .and()
+                .withUser("user2").password(getEncoder().encode("pass2")).roles("USER")
+                .and()
+                .withUser("user3").password(getEncoder().encode("pass3")).roles("ADMIN")
+                .and()
+                .passwordEncoder(getEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
